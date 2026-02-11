4047eb589da4c6d50ab5fc98a757774ec8d2bfd7
Claire Novotny <claire@novotny.org>
2026-02-11 15:27:07 +0000

    refactor: consolidate session option mapper partial files

--- src/cai/Sessions/Options/SessionOptionMapper.cs
+++ src/cai/Sessions/Options/SessionOptionMapper.cs
@@ -11,4 +11,71 @@ internal interface ISessionOptionMapper
     SessionCommandOptions FromExec(ExecCommandOptions options);
 }
 
-internal sealed partial class SessionOptionMapper : ISessionOptionMapper;
+internal sealed class SessionOptionMapper : ISessionOptionMapper
+{
+    public SessionCommandOptions FromRun(RunCommandOptions options)
+        => SessionCommandOptions.Create(SessionMode.Run) with
+        {
+            Workspace = options.Workspace,
+            DataVolume = options.DataVolume,
+            ExplicitConfig = options.Config,
+            Container = options.Container,
+            Template = options.Template,
+            ImageTag = options.ImageTag,
+            Channel = options.Channel,
+            Memory = options.Memory,
+            Cpus = options.Cpus,
+            Credentials = options.Credentials,
+            AcknowledgeCredentialRisk = options.AcknowledgeCredentialRisk,
+            Fresh = options.Fresh,
+            Force = options.Force,
+            Detached = options.Detached,
+            Quiet = options.Quiet,
+            Verbose = options.Verbose,
+            Debug = options.Debug,
+            DryRun = options.DryRun,
+            CommandArgs = options.CommandArgs,
+            EnvVars = [.. options.Env],
+        };
+
+    public SessionCommandOptions FromShell(ShellCommandOptions options)
+        => SessionCommandOptions.Create(SessionMode.Shell) with
+        {
+            Workspace = options.Workspace,
+            DataVolume = options.DataVolume,
+            ExplicitConfig = options.Config,
+            Container = options.Container,
+            Template = options.Template,
+            ImageTag = options.ImageTag,
+            Channel = options.Channel,
+            Memory = options.Memory,
+            Cpus = options.Cpus,
+            Fresh = options.Fresh,
+            Reset = options.Reset,
+            Force = options.Force,
+            Quiet = options.Quiet,
+            Verbose = options.Verbose,
+            Debug = options.Debug,
+            DryRun = options.DryRun,
+            CommandArgs = options.CommandArgs,
+            EnvVars = [],
+        };
+
+    public SessionCommandOptions FromExec(ExecCommandOptions options)
+        => SessionCommandOptions.Create(SessionMode.Exec) with
+        {
+            Workspace = options.Workspace,
+            DataVolume = options.DataVolume,
+            ExplicitConfig = options.Config,
+            Container = options.Container,
+            Template = options.Template,
+            Channel = options.Channel,
+            Fresh = options.Fresh,
+            Force = options.Force,
+            Quiet = options.Quiet,
+            Verbose = options.Verbose,
+            Debug = options.Debug,
+            CommandArgs = options.CommandArgs,
+            EnvVars = [],
+        };
+}
